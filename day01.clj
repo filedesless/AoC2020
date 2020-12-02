6b8c407701e5b0a54c3ed68a9df7b5266904a227
filedesless <filedesless@google.com>
2020-12-02 13:58:26 -0500

    re-write day01 to pick first valid answer

--- day01.clj
+++ day01.clj
@@ -1,10 +1,20 @@
 (def numbers
   (map clojure.edn/read-string (clojure.string/split-lines (slurp "input01.txt"))))
 
-(doseq [i numbers j numbers]
-  (if (= 2020 (+ i j))
-    (println i "*" j "=" (* i j))))
+(def pairs
+  (for [i numbers j numbers]
+    [i j]))
 
-(doseq [i numbers j numbers k numbers]
-  (if (= 2020 (+ i j k))
-    (println i "*" j "*" k "=" (* i j k))))
+(def pair
+  (first (filter #(= 2020 (+ (get % 0) (get % 1))) pairs)))
+
+(println "Day01a:" (* (get pair 0) (get pair 1)))
+
+(def triplets
+  (for [i numbers j numbers k numbers]
+    [i j k]))
+
+(def triplet
+  (first (filter #(= 2020 (+ (get % 0) (get % 1) (get % 2))) triplets)))
+
+(println "Day01b:" (* (get triplet 0) (get triplet 1) (get triplet 2)))
