2cbeed51e17441e83f704a50962fb143285f7187
filedesless <filedesless@google.com>
2020-12-05 20:27:48 -0500

    use short name

--- test/aoc2020/day02_test.clj
+++ test/aoc2020/day02_test.clj
@@ -2,13 +2,12 @@
   (:require [aoc2020.day02 :as sut]
             [clojure.test :as t]))
 
-(clojure.test/testing "old-policy"
-  (clojure.test/is (sut/old-policy 1 3 \a "abcde"))
-  (clojure.test/is (not (sut/old-policy 1 3 \b "cdefg")))
-  (clojure.test/is (sut/old-policy 2 9 \c "ccccccccc")))
+(t/testing "old-policy"
+  (t/is (sut/old-policy 1 3 \a "abcde"))
+  (t/is (not (sut/old-policy 1 3 \b "cdefg")))
+  (t/is (sut/old-policy 2 9 \c "ccccccccc")))
 
-
-(clojure.test/testing "new-policy"
-  (clojure.test/is (sut/new-policy 1 3 \a "abcde"))
-  (clojure.test/is (not (sut/new-policy 1 3 \b "cdefg")))
-  (clojure.test/is (not (sut/new-policy 2 9 \c "ccccccccc"))))
+(t/testing "new-policy"
+  (t/is (sut/new-policy 1 3 \a "abcde"))
+  (t/is (not (sut/new-policy 1 3 \b "cdefg")))
+  (t/is (not (sut/new-policy 2 9 \c "ccccccccc"))))
