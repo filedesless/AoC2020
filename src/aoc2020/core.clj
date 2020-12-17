8373c24a9f4d40a307779008d3710b32c5dcf0bf
filedesless <filedesless@google.com>
2020-12-17 13:07:48 -0500

    fixed test and run

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -3,6 +3,7 @@
   (:require [aoc2020.day01 :as day01])
   (:require [aoc2020.day02 :as day02])
   (:require [aoc2020.day03 :as day03])
+  (:require [aoc2020.day04 :as day04])
   )
 
 (defn -main
@@ -14,4 +15,5 @@
   (println "Day02b:" (day02/day02b))
   (println "Day03a:" (day03/day03a))
   (println "Day03b:" (day03/day03b))
+  (println "Day04a:" (day04/day04a))
   )
--- test/aoc2020/day04_test.clj
+++ test/aoc2020/day04_test.clj
@@ -3,40 +3,40 @@
             [clojure.test :as t]))
 
 (def passeports
-  ({"hgt" "183cm",
-    "pid" "860033327",
-    "byr" "1937",
-    "eyr" "2020",
-    "iyr" "2017",
-    "ecl" "gry",
-    "cid" "147",
-    "hcl" "#fffffd"}
-   {"pid" "028048884",
-    "byr" "1929",
-    "eyr" "2023",
-    "iyr" "2013",
-    "ecl" "amb",
-    "cid" "350",
-    "hcl" "#cfa07d"}
-   {"hgt" "179cm",
-    "pid" "760753108",
-    "byr" "1931",
-    "eyr" "2024",
-    "iyr" "2013",
-    "ecl" "brn",
-    "hcl" "#ae17e1"}
-   {"hgt" "59in",
-    "pid" "166559648",
-    "eyr" "2025",
-    "iyr" "2011",
-    "ecl" "brn",
-    "hcl" "#cfa07d"}))
+  '({"hgt" "183cm",
+     "pid" "860033327",
+     "byr" "1937",
+     "eyr" "2020",
+     "iyr" "2017",
+     "ecl" "gry",
+     "cid" "147",
+     "hcl" "#fffffd"}
+    {"pid" "028048884",
+     "byr" "1929",
+     "eyr" "2023",
+     "iyr" "2013",
+     "ecl" "amb",
+     "cid" "350",
+     "hcl" "#cfa07d"}
+    {"hgt" "179cm",
+     "pid" "760753108",
+     "byr" "1931",
+     "eyr" "2024",
+     "iyr" "2013",
+     "ecl" "brn",
+     "hcl" "#ae17e1"}
+    {"hgt" "59in",
+     "pid" "166559648",
+     "eyr" "2025",
+     "iyr" "2011",
+     "ecl" "brn",
+     "hcl" "#cfa07d"}))
 
 (t/deftest parse
-  (t/is (= (sut/parse "dev-resources/04.txt")) passeports))
+  (t/is (= (sut/parse "dev-resources/04.txt") passeports)))
 
 (t/deftest valid?
-  (t/is (sut/valid? (get passeports 0)))
-  (t/is (not (sut/valid? (get passeports 1))))
-  (t/is (sut/valid? (get passeports 2)))
-  (t/is (sut/valid? (not (get passeports 3)))))
+  (t/is (sut/valid? (nth passeports 0)))
+  (t/is (not (sut/valid? (nth passeports 1))))
+  (t/is (sut/valid? (nth passeports 2)))
+  (t/is (not (sut/valid? (nth passeports 3)))))
